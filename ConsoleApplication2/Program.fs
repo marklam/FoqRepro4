81616d13fc871e98ea5a2c1efb38e2a603b93c3e
Mark Lambert <marklam@spunow.co.uk>
2016-11-23 09:30:24 +0000

    Repro case

---
+++ ConsoleApplication2/Program.fs
@@ -0,0 +1,15 @@
+﻿open Foq
+
+type IFoo =
+    abstract N : int64
+
+module Main =
+    [<EntryPoint>]
+    let main _ = 
+//        let mock1 = Mock<IFoo>().Setup(fun foo -> <@ foo.N @>).Returns(7).Create()
+//        printfn "%d" mock1.N
+
+        let mock2 = Mock<IFoo>().Setup(fun foo -> <@ foo.N @>).ReturnsFunc(fun () -> 7L).Create()
+        printfn "%d" mock2.N
+
+        0
